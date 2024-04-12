d361759057297f52b06aee897825327b1cf4d379
rTexty <kamil.axmadiev88@gmail.com>
2024-04-12 18:05:04 +0500

    Доработан вывод данных

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,8 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
+
+
 back_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
         [
@@ -23,26 +25,121 @@ levels_keyboard = InlineKeyboardMarkup(
 directions_bakalavr = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text='Математика и Информатика', callback_data='math_info')
+            InlineKeyboardButton(text='Социальная работа', callback_data='bac_soc_work')
         ],
         [
-            InlineKeyboardButton(text='Математика и Физика', callback_data='math_physics'),
+            InlineKeyboardButton(text='Организация работы с молодежью', callback_data='bac_org_work'),
+        ],
+        [
+            InlineKeyboardButton(text='Журналистика', callback_data='bac_journalist')
+        ],
+        [
+            InlineKeyboardButton(text='Гостиничное дело', callback_data='bac_hostel')
+        ],
+        [
+            InlineKeyboardButton(text='Педагогическое образование', callback_data='bac_ped')
+        ],
+        [
+            InlineKeyboardButton(text='Специальное (дефектологическое) образование', callback_data='bac_spec')
+        ],
+        [
+            InlineKeyboardButton(text='Документоведение и архивоведение', callback_data='bac_docs')
+        ],
+                [
+            InlineKeyboardButton(text='физическая культура', callback_data='bac_fk')
+        ],
+        [
+            InlineKeyboardButton(text='адаптивная физическая культура', callback_data='bac_afk')
+        ],
+        [
+          InlineKeyboardButton(text='Социально-культурная деятельность', callback_data='bac_soc_culture')
+        ],
+        [
+            InlineKeyboardButton(text='Дизайн', callback_data='bac_design')
+        ],
+                [
+        InlineKeyboardButton(callback_data="Назад", text="Назад", )
         ]
+        
+
     ]
 )
 
 directions_magistr = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text='Цифровизация образования: проектирование, сопровождение, экспертиза ', callback_data='mag_edu')],
+        [
+            InlineKeyboardButton(text='Психология', callback_data='mag_psycho')
+        ],
+                [
+            InlineKeyboardButton(text='Социология', callback_data='mag_soc')
+        ],
+                [
+            InlineKeyboardButton(text='Социальная работа', callback_data='mag_soc_work')
+        ],
+                [
+            InlineKeyboardButton(text='Педагогическое образование', callback_data='mag_ped')
+        ],
+                [
+            InlineKeyboardButton(text='Специальное образование', callback_data='mag_spec')
+        ],
+                [
+            InlineKeyboardButton(text='Документоведение и архивоведение', callback_data='mag_docs')
+        ],
+                [
+            InlineKeyboardButton(text='Антропология и этнология', callback_data='mag_antrop')
+        ],
+                [
+            InlineKeyboardButton(text='Спорт', callback_data='mag_sport')
+        ],
+                [
+        InlineKeyboardButton(callback_data="Назад", text="Назад", )
+        ]
+
     ]
 )
 directions_aspir = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text='Методология и технология профессионального образования', callback_data='aspir_edu')
+            InlineKeyboardButton(text='Физиология человека и животных', callback_data='aspir_fiz')
         ],
         [
-            InlineKeyboardButton(text='Общая педагогика, история педагогики и образования', callback_data='aspir_history'),
+            InlineKeyboardButton(text='Педагогическая психология, психодиагностика цифровых образовательных сред', callback_data='aspir_ped_psycho'),
+        ],
+                [
+            InlineKeyboardButton(text='Социальная структура, социальные институты и процессы', callback_data='aspir_cos')
+        ],
+        [
+            InlineKeyboardButton(text='Отечественная история', callback_data='aspir_history'),
+        ],
+                [
+            InlineKeyboardButton(text='Общая педагогика, история педагогики и образования', callback_data='aspir_ped')
+        ],
+        [
+            InlineKeyboardButton(text='Коррекционная педагогика ', callback_data='aspir_kor_ped'),
+        ],
+        [
+            InlineKeyboardButton(text='Физическая культура и профессиональная физическая подготовка', callback_data='aspir_fk')
+        ],
+        [
+            InlineKeyboardButton(text='Теория и методика спорта', callback_data='aspir_sport'),
+        ],
+        [
+            InlineKeyboardButton(text='Оздоровительная и адаптивная физическая культура', callback_data='aspir_adapt_fk'),
+        ],
+        [
+            InlineKeyboardButton(text='Методология и технология профессионального образования', callback_data='aspir_prof_edu'),
+        ],
+        [
+            InlineKeyboardButton(text='Русская литература и литературы народов Российской Федерации', callback_data='aspir_literature'),
+        ],
+        [
+            InlineKeyboardButton(text='Русский язык. Языки народов России', callback_data='aspir_russian'),
+        ],      
+        [
+            InlineKeyboardButton(text='Теоретическая, прикладная и сравнительно-сопоставительная лингвистика', callback_data='aspir_lingv'),
+        ],
+        [
+        InlineKeyboardButton(callback_data="Назад", text="Назад", )
         ]
     ]
 )
